f7da42aba46d7d871e42561ce075c7e01f10a26e
Michael Koch <michaelkoch4444@gmail.com>
2023-08-03 07:50:21 -0400

    Commiting code

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
+import {MoodNft} from "../src/MoodNft.sol";
 
 contract MintBasicNft is Script {
     string public constant PUG_URI =
@@ -21,6 +22,38 @@ contract MintBasicNft is Script {
     function mintNftOnContract(address basicNftAddress) public {
         vm.startBroadcast();
         BasicNft(basicNftAddress).mintNft(PUG_URI); // this is wrapping the address given as a NasicNft. Now we can interact with the contract as if it is a BasicNft
+        vm.stopBroadcast();
+    }
+}
+
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "MoodNft",
+            block.chainid
+        );
+        mintMoodNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintMoodNftOnContract(address moodNftAddress) public {
+        vm.startBroadcast();
+        MoodNft(moodNftAddress).mintNft();
+        vm.stopBroadcast();
+    }
+}
+
+contract FlipMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "MoodNft",
+            block.chainid
+        );
+        flipMoodOfMoodNftOnContract(mostRecentlyDeployed);
+    }
+
+    function flipMoodOfMoodNftOnContract(address moodNftAddress) public {
+        vm.startBroadcast();
+        MoodNft(moodNftAddress).flipMood(0);
         vm.stopBroadcast();
     }
 }
--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -5,6 +5,8 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
+    error MoodNft__CantFlipMoodIfNotOwner();
+
     uint256 private s_tokenCounter;
     string private s_sadSvgImageURI;
     string private s_happySvgImageURI;
@@ -31,6 +33,22 @@ contract MoodNft is ERC721 {
         s_tokenCounter++;
     }
 
+    function flipMood(uint256 tokenId) public {
+        //only want the NFT owner to be able to change the mood
+        if (!_isApprovedOrOwner(msg.sender, tokenId)) {
+            revert MoodNft__CantFlipMoodIfNotOwner();
+        }
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
     // Base64 util allows you to transform bytes32 data into its Base64 string representation.
     // were first going to make our metadata json object, and then we're going to use openzeppelin to convert this Json object into Json tokenURI
     function tokenURI(
@@ -43,18 +61,31 @@ contract MoodNft is ERC721 {
             imageURI = s_sadSvgImageURI;
         }
 
-        // this is equivalent to getting the giant text string that we got from running the base64 terminal command
-        Base64.encode(
-            bytes(
+        return
+            string(
                 abi.encodePacked(
-                    '{"name":"',
-                    name(), // You can add whatever name here
-                    '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
-                    '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
-                    imageURI,
-                    '"}'
+                    _baseURI(),
+                    Base64.encode( // this is equivalent to getting the giant text string that we got from running the base64 terminal command
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(), // You can add whatever name here
+                                '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                                '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
                 )
-            )
-        );
+            );
+    }
+
+    function getTokenIdToMood(uint256 tokenId) external view returns (Mood) {
+        return s_tokenIdToMood[tokenId];
+    }
+
+    function getTokenCounter() external view returns (uint256) {
+        return s_tokenCounter;
     }
 }
